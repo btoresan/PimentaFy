37eae225d6d1dfffad0edb5b77641353d7837d75
btoresan <btoresan@gmail.com>
2024-11-07 16:22:50 -0300

    Fixed Main Sound bug

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Sound> music = new ArrayList<Sound>();
+        ArrayList<Sound> music = new ArrayList<>();
         Sound sound = new Sound();
 
         for(int i=0; i < 100; i++) {
             sound.instrument = 2;
-            sound.pitch = (int) 2*i;
+            sound.pitch =  2*i;
             sound.volume = 1000;
 
             music.add(sound);
