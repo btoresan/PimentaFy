abf24a0a76f9fe16e16f86aae94a0516a7f6d484
btoresan <btoresan@gmail.com>
2024-11-07 19:15:19 -0300

    Privatize some methods

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -9,11 +9,13 @@ public class MusicPlayer {
     private int noteDuration;
     private ArrayList<Sound> music;
 
+    //Constructor Class
     public MusicPlayer(ArrayList<Sound> music, int userBPM){
         setMusic(music);
         setBPM(userBPM);
     }
 
+    //Iterate through the sounds in the music and plays them
     public void play() {
         startSynthesizer();
 
@@ -28,7 +30,17 @@ public class MusicPlayer {
         closeSynthesizer();
     }
 
-    public void startSynthesizer() {
+    //Allows user to change the music without creating a new instance
+    public void setMusic(ArrayList<Sound> userMusic) {
+        music = userMusic;
+    }
+
+    //Allows user to change the BPM without creating a new instance
+    public void setBPM(int userBPM) {
+        noteDuration = 60000/(userBPM);
+    }
+
+    private void startSynthesizer() {
         try {
             synthesizer = MidiSystem.getSynthesizer();
             synthesizer.open(); // Open the synthesizer here to use later
@@ -37,21 +49,13 @@ public class MusicPlayer {
         }
     }
 
-    public void closeSynthesizer() {
+    private void closeSynthesizer() {
         if (synthesizer != null && synthesizer.isOpen()) {
             synthesizer.close();
         }
     }
 
-    public void setMusic(ArrayList<Sound> userMusic) {
-        music = userMusic;
-    }
-
-    public void setBPM(int userBPM) {
-        noteDuration = 60000/(userBPM);
-    }
-
-    public void playNote(int pitch, int volume, int instrument) {
+    private void playNote(int pitch, int volume, int instrument) {
         try {
             // Get a MIDI channel to play notes
             MidiChannel[] channels = synthesizer.getChannels();
